c8c00d2d2677ce85a785675d8cd2e4d4a5f0fa7a
TahirUddinAhmed <01tahirahmed@gmail.com>
2025-08-03 22:22:02 +0530

    Add factories for Book and Review models, and update DatabaseSeeder to generate reviews with varying ratings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Book;
+use App\Models\Review;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // generate 100 books and each of those have at least 5 reviews, but not more than 30
+        Book::factory(33)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+
+            Review::factory()->count($numOfReviews)->good()->for($book)->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+
+            Review::factory()->count($numOfReviews)->average()->for($book)->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numOfReviews = random_int(5, 30);
+
+            Review::factory()->count($numOfReviews)->bad()->for($book)->create();
+        });
+
     }
 }
